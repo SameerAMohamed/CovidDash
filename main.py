31872acbf94703a7530d7c1e111d57f04113b81c
Sameer Mohamed <36165297+SameerAMohamed@users.noreply.github.com>
2021-03-20 13:25:28 -0500

    Update main.py
    Changed button sizes and heading.

--- main.py
+++ main.py
@@ -11,9 +11,9 @@ app = dash.Dash(__name__)  # Start the app
 
 # ---------- Import and clean data (importing csv into pandas)
 df = pd.read_csv("owid-covid-data.csv")  # read data into dataframe
-
-df_refined = df[['date', 'continent', 'location', 'total_cases', 'new_cases', 'total_deaths', 'new_deaths',
-                 'total_cases_per_million', 'new_cases_per_million']]
+headings = ['date', 'continent', 'location', 'total_cases', 'new_cases', 'total_deaths', 'new_deaths',
+                 'total_cases_per_million', 'new_cases_per_million']
+df_refined = df[headings]
 df_refined = df_refined.fillna(0)  # Fill all empty cells with 0
 
 df_refined.reset_index(inplace=True)  # Reset index for alignment
@@ -24,28 +24,32 @@ name_arr = df_refined.location.unique()  # Make an array out of the names
 # Now for app layout
 app.layout = html.Div([
 
-    html.H1("Web Application Dashboards with Dash", style={'text-align': 'center'}),  # title for the webpage header
-
-    dcc.Dropdown(  # First button for the country selection
-        id="country_input",
-        options=[{'label': x, 'value': x} for x in name_arr],
-        value='',
-        placeholder='Select a country',
-        searchable=True,
-        multi=True
-    ),
-    dcc.Dropdown(  # Second button for the metric selection
-        id="metric_input",
-        options=[{'label': 'Total Cases', 'value': 'total_cases'},
-                 {'label': 'New Cases', 'value': 'new_cases'},
-                 {'label': 'Total Deaths', 'value': 'total_deaths'},
-                 {'label': 'New Deaths', 'value': 'new_deaths'},
-                 {'label': 'Total Cases Per Million', 'value': 'total_cases_per_million'},
-                 {'label': 'New Cases Per Person', 'value': 'new_cases_per_million'}],
-        value='',
-        placeholder='Select a Metric',
-        searchable=True,
-        multi=False
+    html.H1("WebApp for COVID Using Dash", style={'text-align': 'center'}),  # title for the webpage header
+    html.Div(
+        [
+            dcc.Dropdown(  # First button for the country selection
+                id="country_input",
+                options=[{'label': x, 'value': x} for x in name_arr],
+                value='',
+                placeholder='Select a country',
+                searchable=True,
+                multi=True
+            ),
+            dcc.Dropdown(  # Second button for the metric selection
+                id="metric_input",
+                options=[{'label': 'Total Cases', 'value': 'total_cases'},
+                         {'label': 'New Cases', 'value': 'new_cases'},
+                         {'label': 'Total Deaths', 'value': 'total_deaths'},
+                         {'label': 'New Deaths', 'value': 'new_deaths'},
+                         {'label': 'Total Cases Per Million', 'value': 'total_cases_per_million'},
+                         {'label': 'New Cases Per Person', 'value': 'new_cases_per_million'}],
+                value='',
+                placeholder='Select a Metric',
+                searchable=True,
+                multi=False
+            ),
+        ],
+        style={"width": "30%"},
     ),
 
     html.Div(id='output_container', children=[]),
